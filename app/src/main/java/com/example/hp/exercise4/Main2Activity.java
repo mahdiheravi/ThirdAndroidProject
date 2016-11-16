5eed9768d6d067611d4dacf10ae7789466547f6f
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-11-17 01:43:43 +0330

    finalizing Regester form and intent

--- app/src/main/java/com/example/hp/exercise4/Main2Activity.java
+++ app/src/main/java/com/example/hp/exercise4/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.hp.exercise4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent myintent = getIntent();
+        String name =   myintent.getStringExtra("myname");
+        String family = myintent.getStringExtra("myfamily");
+        String email = myintent.getStringExtra("myemail");
+        int age = myintent.getIntExtra("myage",0);
+
+        EditText myname = (EditText) findViewById(R.id.name);
+        myname.setText(name);
+
+        EditText myemail = (EditText) findViewById(R.id.email);
+        myemail.setText(name);
+
+        EditText myfamily = (EditText) findViewById(R.id.family);
+        myfamily.setText(name);
+
+        EditText myage = (EditText) findViewById(R.id.age);
+        myage.setText(String.valueOf(age));
+
+
     }
 }
